f4a820ca9e3b0b9f0c07b46e1f2308a414b41ed9
Dana Zack <danaezack@gmail.com>
2024-01-15 13:38:50 -0700

    Refactor: update syntax per Turing style guidelines

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,38 +1,32 @@
 import { getData, postBooking, deleteBooking } from './apiCalls'
-import {
-  gatherBookingsByCustomer,
-  calculateCosts,
-  findRoomsByDate,
-  filterRoomsByType,
-  addBooking
-} from './rooms'
+import { gatherBookingsByCustomer, calculateCosts, findRoomsByDate, filterRoomsByType, addBooking } from './rooms'
 
 // Selectors: views
-let loginView = document.querySelector('.login-view')
-let welcomeView = document.querySelector('.welcome-view')
-let myBookingsView = document.querySelector('.my-bookings-view')
-let searchView = document.querySelector('.search-view')
-let roomsView = document.querySelector('.rooms-view')
-let confirmationView = document.querySelector('.confirmation-view')
+let loginView = document.querySelector('.login-view');
+let welcomeView = document.querySelector('.welcome-view');
+let myBookingsView = document.querySelector('.my-bookings-view');
+let searchView = document.querySelector('.search-view');
+let roomsView = document.querySelector('.rooms-view');
+let confirmationView = document.querySelector('.confirmation-view');
 
 // Selectors: buttons
-let loginBtn = document.querySelector('#login-btn')
-let myBookingsBtn = document.querySelector('#nav-bookings-btn')
-let homeBtn = document.querySelector('#nav-home-btn')
-let bookStayBtn = document.querySelector('#nav-rooms-btn')
-let searchBtn = document.querySelector('#search-btn')
-let input = document.querySelector('#date')
+let loginBtn = document.querySelector('#login-btn');
+let myBookingsBtn = document.querySelector('#nav-bookings-btn');
+let homeBtn = document.querySelector('#nav-home-btn');
+let bookStayBtn = document.querySelector('#nav-rooms-btn');
+let searchBtn = document.querySelector('#search-btn');
 
-// Selectors: additional
-let availableRoomsSection = document.querySelector('.available-cards-container')
-let myBookingsSection = document.querySelector('.my-bookings-container')
-let availableRoomsTitle = document.querySelector('.available-rooms-title')
-let welcomeMessage = document.querySelector('.welcome-message')
-let noRoomsMessage = document.querySelector('.no-rooms-message')
-let noBookingsMessage = document.querySelector('.no-bookings-message')
-let confirmationMessage = document.querySelector('.confirmation-message')
-let filterMenu = document.querySelector('#filter-drop-down')
-let navBar = document.querySelector('nav')
+// Selectors: other
+let navBar = document.querySelector('nav');
+let availableRoomsSection = document.querySelector('.available-cards-container');
+let myBookingsSection = document.querySelector('.my-bookings-container');
+let welcomeMessage = document.querySelector('.welcome-message');
+let noRoomsMessage = document.querySelector('.no-rooms-message');
+let noBookingsMessage = document.querySelector('.no-bookings-message');
+let confirmationMessage = document.querySelector('.confirmation-message');
+let availableRoomsTitle = document.querySelector('.available-rooms-title');
+let filterMenu = document.querySelector('#filter-drop-down');
+let dateInput = document.querySelector('#date');
 
 // Variables
 let customer;
@@ -44,7 +38,6 @@ let currentRoom;
 let availableRooms;
 
 // Event Listeners
-
 window.addEventListener('load', () => {
   getUser(50);
   getRooms();
@@ -52,83 +45,82 @@ window.addEventListener('load', () => {
 })
 
 loginBtn.addEventListener('click', () => {
-  switchToView(welcomeView)
+  switchToView(welcomeView);
   welcomeMessage.innerText = `Welcome, ${customer.name}`;
 })
 
 myBookingsBtn.addEventListener('click', () => {
-  switchToView(myBookingsView)
+  switchToView(myBookingsView);
 })
 
 homeBtn.addEventListener('click', () => {
-  switchToView(welcomeView)
+  switchToView(welcomeView);
 })
 
 bookStayBtn.addEventListener('click', () => {
-  switchToView(searchView)
-  input.value = ''
+  switchToView(searchView);
+  dateInput.value = '';
 })
 
 searchBtn.addEventListener('click', (event) => {
-  event.preventDefault()
-  if (!input.value) {
-    return
+  event.preventDefault();
+  if (!dateInput.value) {
+    return;
   }
-  if (checkDateValidity(input.value)) {
-    return
+  if (checkDateValidity(dateInput.value)) {
+    return;
   }
-  selectedDate = input.value.replaceAll("-", "/")
-  switchToView(roomsView)
-  availableRoomsTitle.innerText = `Rooms available on ${selectedDate}`
-  availableRooms = findRoomsByDate(selectedDate, rooms, bookings)
-  displayRooms(availableRooms)
+  selectedDate = dateInput.value.replaceAll("-", "/");
+  switchToView(roomsView);
+  availableRoomsTitle.innerText = `Rooms available on ${selectedDate}`;
+  availableRooms = findRoomsByDate(selectedDate, rooms, bookings);
+  displayRooms(availableRooms);
 })
 
 availableRoomsSection.addEventListener('click', (event) => {
   if (event.target.closest('button')) {
-    selectedRoom = event.target.closest('article')
-    completeBooking()
-    switchToView(confirmationView)
-    displayBookingConfirmation()
+    selectedRoom = event.target.closest('article');
+    completeBooking();
+    switchToView(confirmationView);
+    displayBookingConfirmation();
   }
 });
+
+filterMenu.addEventListener('change', () => {
+  console.log(filterMenu.value);
+  let filteredRooms = filterRoomsByType(filterMenu.value, availableRooms);
+  displayRooms(filteredRooms);
+})
 
 // myBookingsSection.addEventListener('click', (event) => {
 //   selectedRoom = event.target.closest('article');
 //   //run function to 'delete' selected room as a booking
-//   // console.log(selectedRoom)
+//   // console.log(selectedRoom);
 // });
 
-filterMenu.addEventListener('change', () => {
-  console.log(filterMenu.value)
-  let filteredRooms = filterRoomsByType(filterMenu.value, availableRooms)
-  displayRooms(filteredRooms)
-})
-
 // Functions
-
 function completeBooking() {
-  updateCurrentRoom(selectedRoom)
-  const newBooking = addBooking(customer, currentRoom, selectedDate)
+  updateCurrentRoom(selectedRoom);
+  const newBooking = addBooking(customer, currentRoom, selectedDate);
   postBooking(newBooking);
   getBookings();
-  displayBookingConfirmation()
+  displayBookingConfirmation();
 }
 
 function updateCurrentRoom(selectedRoom) {
-  const selectedRoomNumber = Number(selectedRoom.querySelector('.roomNumber').innerText)
+  const selectedRoomNumber = Number(selectedRoom.querySelector('.roomNumber').innerText);
   currentRoom = rooms.filter(room => {
-    return room.number === selectedRoomNumber
-  })[0]
+    return room.number === selectedRoomNumber;
+  })[0];
 }
 
 function displayRooms(availableRooms) {
   availableRoomsSection.innerHTML = '';
   if (availableRooms.length === 0) {
-    noRoomsMessage.classList.remove('hidden')
+    noRoomsMessage.classList.remove('hidden');
   } else {
     availableRooms.forEach(room => {
-      noRoomsMessage.classList.add('hidden')
+      noRoomsMessage.classList.add('hidden');
       availableRoomsSection.innerHTML += `
       <article class="available-card">
         <h3>Room #${room.number}</h3>
@@ -141,28 +133,28 @@ function displayRooms(availableRooms) {
           <p class="roomNumber hidden">${room.number}</p>
         </div>
         <button class="book-btn">Book this room</button>
-      </article>`
+      </article>`;
     });
   }
 }
 
 function displayBookingConfirmation() {
-  confirmationMessage.innerText = `You have successfully booked room #${currentRoom.number} for the evening of ${selectedDate}. We can't wait to see you then!`
+  confirmationMessage.innerText = `You have successfully booked room #${currentRoom.number} for the evening of ${selectedDate}. We can't wait to see you then!`;
 }
 
 function checkDateValidity(date) {
-  const userInput = new Date(date)
-  const currentDate = new Date()
-  console.log(userInput)
-  console.log(currentDate)
-  currentDate.setHours(0, 0, 0, 0)
-  return userInput < currentDate
+  const userInput = new Date(date);
+  const currentDate = new Date();
+  console.log(userInput);
+  console.log(currentDate);
+  // currentDate.setHours(0, 0, 0, 0);
+  return userInput < currentDate;
 }
 
 function switchToView(view) {
   hideAllViews();
-  show(view)
-  show(navBar)
+  show(view);
+  show(navBar);
 }
 
 function hideAllViews() {
@@ -175,17 +167,14 @@ function hideAllViews() {
 }
 
 function hide(element) {
-  element.classList.add('hidden')
+  element.classList.add('hidden');
 }
 
 function show(element) {
-  element.classList.remove('hidden')
+  element.classList.remove('hidden');
 }
 
-
-
 // Data Retrieval & Assignment
-
 function getUser(id) {
   getData(`http://localhost:3001/api/v1/customers/${id}`)
     .then(data => {
@@ -210,15 +199,8 @@ function getBookings() {
     })
 }
 
-// function getAllUsers() {
-//   getData('http://localhost:3001/api/v1/customers')
-//     .then(data => console.log(data))
-// }
-
 // ============================================================
-
 export {
-  // getAllUsers,
   getUser,
   getRooms,
   getBookings
